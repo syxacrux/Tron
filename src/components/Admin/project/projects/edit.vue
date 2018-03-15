002e3e293f51739efddde5ca83ae6783ae363247
songyuxin <song.yuxin@wifun.net>
2018-03-15 15:56:40 +0800

    编辑项目 syx 20180315

--- src/components/Admin/project/projects/edit.vue
+++ src/components/Admin/project/projects/edit.vue
@@ -403,7 +403,7 @@
         this.$refs.form.validate((pass) => {
           if (pass) {
             this.isLoading = !this.isLoading
-            this.apiPut('admin/projects', this.id, this.form).then((res) => {
+            this.apiPut('admin/projects/', this.id, this.form).then((res) => {
               this.handelResponse(res, (data) => {
                 _g.toastMsg('success', '编辑成功')
                 _g.clearVuex('setUsers')
