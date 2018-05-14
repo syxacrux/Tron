cf4838a6ec41bc8535d0d4959a80f4c4bb9ba93b
Zjs <zhaojiusiphp@163.com>
2018-05-14 12:01:30 +0800

    help category

--- 1.php
+++ 1.php
@@ -28,12 +28,12 @@ date_default_timezone_set('PRC'); //设置中国时区
 //		}
 //	}
 //}
-$log_path = 'runtime/log/'.date("Ym");
-if(is_dir($log_path)){
-	$log_file_name = $log_path.DIRECTORY_SEPARATOR.'python_'.date('d').'.txt';
-	$res_content = 'time:'.date("Y-m-d H:i:s").'   '.'123';
-	file_put_contents("$log_file_name",$res_content.PHP_EOL,FILE_APPEND);
-}else{
-	echo 2;
-}
+//$log_path = 'runtime/log/'.date("Ym");
+//if(is_dir($log_path)){
+//	$log_file_name = $log_path.DIRECTORY_SEPARATOR.'python_'.date('d').'.txt';
+//	$res_content = 'time:'.date("Y-m-d H:i:s").'   '.'123';
+//	file_put_contents("$log_file_name",$res_content.PHP_EOL,FILE_APPEND);
+//}else{
+//	echo 2;
+//}
 //file_put_contents('runtime/log/201805/aa.txt','123');
