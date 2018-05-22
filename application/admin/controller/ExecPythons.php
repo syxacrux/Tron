b787854d5c631b97098e3e2bdc2f2adfbb1c18f2
Zjs <zhaojiusiphp@163.com>
2018-05-22 13:49:07 +0800

    exec python zjs

---
+++ application/admin/controller/ExecPythons.php
@@ -0,0 +1,24 @@
+<?php
+// +----------------------------------------------------------------------
+// | Description: 测试 对外接口 无验证权限
+// +----------------------------------------------------------------------
+// | Author: zjs <linchuangbin@honraytech.com>
+// +----------------------------------------------------------------------
+
+namespace app\admin\controller;
+
+use app\common\controller\Common;
+
+class ExecPythons extends Common
+{
+	//测试
+	public function get_test()
+	{
+		$model = model('Test');
+		$param = $this->param;
+		$data = $model->getTestList($param);
+		return resultArray(['data' => $data]);
+	}
+
+
+}
